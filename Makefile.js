a940cff848c9b56026a87850542ac445ba7a61c2
Rodrigo Campos <rod.apd@gmail.com>
2022-12-19 20:16:38 -0300

    feat: load chars on merchant

--- Makefile.js
+++ Makefile.js
@@ -17,7 +17,7 @@ const filenames = chars.map(({ name, id }) => path.resolve(__dirname, "dist", `$
 
 // TODO: You need to replace this value!
 const ADVENTURE_LAND_CODES_PATH =
-    "path/to/Adventure Land/autosync0123465789/adventureland/characters";
+    "/home/rod/.config/Adventure Land/autosync6382155688574976/adventureland/characters";
 
 /** This command will push your files to your remote server. */
 // TODO: If you want to use this command, you have to fill the server and path!
